086e04a89960846273224156db9595abeb098ae6
ADRIELmg0205 <adrielgraupp0205@gmail.com>
2024-04-27 17:16:48 -0300

    fuçando nos menus

---
+++ src/components/header/BottomHeader.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { LuMenu } from 'react-icons/lu'
+
+const BottomHeader = () => {
+    return(
+        <div className="flex justify-center items-center w-full h-10 bg-Azul text-sm text-white px-4">
+            <p className="hidden md:inline-flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300 text-white mx-4"> 
+                Cervejas 
+            </p>
+            <p className="hidden md:inline-flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300 text-white mx-4"> 
+                Destilados 
+            </p>
+            <p className="hidden md:inline-flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300 text-white mx-4"> 
+                Vinhos 
+            </p>
+            <p className="hidden md:inline-flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300 text-white mx-4"> 
+                Refrigerantes 
+            </p>
+            <p className="hidden md:inline-flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300 text-white mx-4"> 
+                Energéticos 
+            </p>
+            <p className="hidden md:inline-flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300 text-white font-bold mx-4"> 
+                Clube de Assinatura 
+            </p>
+        </div>
+    )
+}
+
+export default BottomHeader
