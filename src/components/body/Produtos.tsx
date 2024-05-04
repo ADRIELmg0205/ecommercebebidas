4c69117cdca3dd8a3c782c4f82e587fa38b798ef
ADRIELmg0205 <adrielgraupp0205@gmail.com>
2024-05-04 17:58:08 -0300

    Aprsentando produtos cadastrados no banco

--- src/components/body/Produtos.tsx
+++ src/components/body/Produtos.tsx
@@ -1,26 +1,58 @@
-import Image from "next/image"
-import veibarrero from "../../images/veibarrero.png"
+import { useState, useEffect } from 'react';
+import Image from "next/image";
 
-export default function Produtos() {
-    return (
-        <div className="flex flex-col items-center rounded-md border border-zinc-500 h-80 w-56 ">
-           
-                <h2 className="text-lg text-center font-semibold p-4">Veio Barreiro</h2>
-            
-            <Image
-                src={veibarrero}
-                height={50} width={50}
-                alt={"veibarrero.png"}
-                className="" />
-            <span className="flex justify-between text-center p-4">R$19,99 </span>
-            <span className="text-xs">Aguardente Velho Barreiro 980ml</span>
-            <button className={`
-                 bg-blue-500 h-7 w-3/4 border-none text-white 
-                text-base font-bold roudend-md mt-2
-            `}>Saiba Mais</button>
-
-        </div>
-    )
+interface Produto {
+    id_produto: number;
+    nome_produto: string;
+    preco_produto: string;
+    descricao_produto: string;
+    tipo_produto: string;
+    imagem_produto: string;
 }
 
+interface Props {
+    apiUrl: string; // Defina a propriedade apiUrl como uma string
+}
 
+export default function Produtos({ apiUrl }: Props) {
+    const [produtos, setProdutos] = useState<Produto[]>([]);
+
+    useEffect(() => {
+        async function fetchProdutos() {
+            try {
+                const response = await fetch(apiUrl); // Use o caminho fornecido como propriedade
+                if (response.ok) {
+                    const data = await response.json();
+                    setProdutos(data.result.rows);
+                } else {
+                    console.error('Erro ao buscar produtos:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Erro ao buscar produtos:', error);
+            }
+        }
+
+        fetchProdutos();
+    }, [apiUrl]); // Certifique-se de adicionar apiUrl como dependência
+
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            {produtos.map(produto => (
+                <div key={produto.id_produto} className="flex flex-col items-center rounded-md border border-zinc-500 h-80 w-56">
+                    <h2 className="text-lg text-center font-semibold p-4">{produto.nome_produto}</h2>
+                    <Image
+                        src={produto.imagem_produto} // Altere aqui para o link da imagem do produto
+                        height={100} width={100} // Ajuste o tamanho conforme necessário
+                        alt={produto.nome_produto} // Use o nome do produto como alt
+                        className="" />
+                    <span className="flex justify-between text-center p-4">R${produto.preco_produto}</span>
+                    <span className="text-xs">{produto.descricao_produto}</span>
+                    <button className={`
+                        bg-blue-500 h-7 w-3/4 border-none text-white 
+                        text-base font-bold rounded-md mt-2
+                    `}>Saiba Mais</button>
+                </div>
+            ))}
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,21 +10,32 @@ export default function Home() {
   return (
     <main>
       <Header/>
-      <BottomHeader/>
-
+      <BottomHeader></BottomHeader>
+      
+      <br></br>
+      <h1 id="Geral" className="flex justify-center flex-wrap gap-4 mx-auto max-w-screen-2xl text-2xl font-bold text-Azul ">GERAL</h1>
       <div className="flex justify-center flex-wrap gap-4 mx-auto max-w-screen-2xl ">
-        <Produtos/>
-        <Produtos/>
-        <Produtos/>
-        <Produtos/>
-        <Produtos/>
-        <Produtos/>
-        <Produtos/>
-        <Produtos/>
+        <Produtos apiUrl="/api/Geral" />
       </div>
-      
-      
+      <br></br>
+
+      <section id="cervejas">
+      <h1 id="#Cervejas" className="flex justify-center flex-wrap gap-4 mx-auto max-w-screen-2xl text-2xl font-bold text-Azul">CERVEJAS</h1>
+      <div className="flex justify-center flex-wrap gap-4 mx-auto max-w-screen-2xl ">
+      <Produtos apiUrl="/api/cervejas" />
+      </div>
+      <br></br>
+      </section>
+
+      <h1 id="whiskys" className="flex justify-center flex-wrap gap-4 mx-auto max-w-screen-2xl text-2xl font-bold text-Azul">WHISKY</h1>
+      <div className="flex justify-center flex-wrap gap-4 mx-auto max-w-screen-2xl ">
+          <Produtos apiUrl="/api/whisky" />
+      </div>
+
+      <h1 id="vodkas" className="flex justify-center flex-wrap gap-4 mx-auto max-w-screen-2xl text-2xl font-bold text-Azul">VODKAS</h1>
+      <div className="flex justify-center flex-wrap gap-4 mx-auto max-w-screen-2xl ">
+          <Produtos apiUrl="/api/vodkas" />
+      </div>
     </main>
-    
   );
 }
