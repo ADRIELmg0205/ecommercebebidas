2be76b9aac916478595010a5c1548d0cccdeab2f
ADRIELmg0205 <adrielgraupp0205@gmail.com>
2024-05-27 00:15:41 -0300

    Atualizando Layout do cadastro de Produtos

--- src/pages/cadastra_produtos.tsx
+++ src/pages/cadastra_produtos.tsx
@@ -19,13 +19,14 @@ const Home = () => {
   const [produtos, setProdutos] = useState<Produto[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [produtoToDelete, setProdutoToDelete] = useState<number | null>(null);
+  const [hoveredCard, setHoveredCard] = useState<number | null>(null); // Estado para controlar o hover
   const router = useRouter();
   const [category, setCategory] = useState<string>('todos');
+
   const resetFilters = () => {
     setSearchTerm('');
     setCategory('todos');
   };
-
 
   useEffect(() => {
     const fetchProdutos = async () => {
@@ -65,40 +66,50 @@ const Home = () => {
     <main>
       <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} resetFilters={resetFilters} />
       <div><h1 className="flex justify-center flex-wrap gap-4 mx-auto max-w-screen-2xl text-2xl font-bold text-Azul ">LISTA DE PRODUTOS</h1></div>
+      <br></br>
       <div className="flex justify-center mb-4">
-        <Link href="/new-product">
-          <button className='text-sm flex flex-col justify-center px-2 border border-gray-400 hover:border-black cursor-pointer duration-300 h-[95%] rounded-md px-3 py-2 '>
-            Adicionar Novo Produto
-          </button> 
-        </Link>
+      <Link href="/new-product">
+        <button 
+          className='text-sm flex flex-col justify-center px-2 border border-gray-400 hover:border-black cursor-pointer duration-300 h-[95%] rounded-md px-3 py-2 bg-blue-500 text-white hover:bg-green-500 transform hover:scale-110'
+        >
+          Adicionar Novo Produto
+        </button> 
+      </Link>
       </div>
+      <br></br>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 flex justify-center flex-wrap gap-4 mx-auto max-w-screen-2xl ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 flex justify-center flex-wrap gap-4 mx-auto max-w-screen-2xl ">
         {filteredProdutos.map(produto => (
-          <div key={produto.id_produto} className="flex flex-col items-center rounded-md border border-zinc-500 h-80 w-56">
+          <div 
+            key={produto.id_produto} 
+            className={`flex flex-col items-center rounded-md border border-zinc-500 h-80 w-56 ${hoveredCard === produto.id_produto ? 'transform scale-110 transition-transform' : ''}`} // Adicionando classe CSS com base no hover
+            onMouseEnter={() => setHoveredCard(produto.id_produto)} // Define o card atual como hover
+            onMouseLeave={() => setHoveredCard(null)} // Limpa o estado de hover ao deixar o card
+          >
             <h2 className="text-lg text-center font-semibold p-4">{produto.nome_produto}</h2>
             <Image
               src={produto.imagem_produto}
-              height={70}
-              width={70}
+              height={90}
+              width={90}
               alt={produto.nome_produto}
               className=""
             />
             <span className="flex justify-between text-center p-4">R${produto.preco_produto}</span>
-            <span className="text-xs">{produto.descricao_produto}</span>
-            <p className="text-xs">Tipo: {produto.tipo_produto}</p>
-            {produtoToDelete === produto.id_produto ? (
-              <>
-                <p>Você tem certeza que deseja deletar este produto?</p>
-                <button onClick={handleDeleteProduto} className="bg-red-500 h-7 w-3/4 border-none text-white text-base font-bold rounded-md mt-2">Confirmar</button>
-                <button onClick={() => setProdutoToDelete(null)} className="bg-gray-500 h-7 w-3/4 border-none text-white text-base font-bold rounded-md mt-2">Cancelar</button>
-              </>
-            ) : (
-              <>
-                <button onClick={() => handleEditProduto(produto.id_produto)} className="bg-yellow-500 h-7 w-3/4 border-none text-white text-base font-bold rounded-md mt-2">Editar</button>
-                <button onClick={() => setProdutoToDelete(produto.id_produto)} className="bg-red-500 h-7 w-3/4 border-none text-white text-base font-bold rounded-md mt-2">Deletar</button>
-              </>
-            )}
+            <span className="text-xs p-4">{produto.descricao_produto}</span> {/* Adicionando espaçamento interno */}
+            <div className="flex justify-center mt-auto space-x-2"> {/* Contêiner flexível para os botões */}
+              {produtoToDelete === produto.id_produto ? (
+                <>
+                  <button onClick={handleDeleteProduto} className="bg-red-500 border-none text-white text-base font-bold rounded-md px-3 py-1">Confirmar</button>
+                  <button onClick={() => setProdutoToDelete(null)} className="bg-gray-500 border-none text-white text-base font-bold rounded-md px-3 py-1">Cancelar</button>
+                </>
+              ) : (
+                <>
+                  <button onClick={() => handleEditProduto(produto.id_produto)} className="bg-yellow-500 border-none text-white text-base font-bold rounded-md px-6 py-1">Editar</button>
+                  <button onClick={() => setProdutoToDelete(produto.id_produto)} className="bg-red-500 border-none text-white text-base font-bold rounded-md px-6 py-1">Deletar</button>
+                </>
+              )}
+            </div>
+            <br></br>
           </div>
         ))}
       </div>         
